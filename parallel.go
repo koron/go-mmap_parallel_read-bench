861b1f16f39145095099634beaca703c8fd2768b
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-07-21 14:56:40 +0900

    implemented main logic

--- parallel.go
+++ parallel.go
@@ -1,45 +1,118 @@
 package main
 
 import (
+	"github.com/koron/jvgrep/mmap"
+	"io/ioutil"
 	"log"
+	"math/rand"
+	"os"
 )
 
 const (
-	BigFile = "big.txt"
+	bigFile  = "big.txt"
+	bigSize  = 100 * 1024 * 1024 // =100MB
+	seed     = 123456
+	unitSize = bigSize / 4
 )
 
-func prepareBigFile() {
-	// TODO:
-	log.Println("prepareBigFile")
+func randomData(size int64) []byte {
+	b := make([]byte, size)
+	r := rand.New(rand.NewSource(seed))
+	for i, _ := range b {
+		b[i] = byte(r.Intn(256))
+	}
+	return b
 }
 
-func sequentialRead() {
-	// TODO:
-	log.Println("sequentialRead")
+func prepareBigFile() error {
+	// Check big data file existence.
+	fi, err := os.Stat(bigFile)
+	if err == nil {
+		if fi.Size() == bigSize {
+			return nil
+		}
+	}
+
+	// Create a new big data file
+	log.Println("creating big data file")
+	return ioutil.WriteFile(bigFile, randomData(bigSize), 0644)
 }
 
-func parallelRead() {
-	// TODO:
-	log.Println("parallelRead")
+func countZero(b []byte) int {
+	cnt := 0
+	for _, d := range b {
+		if d == 0 {
+			cnt++
+		}
+	}
+	return cnt
 }
 
-func warmup(f func()) {
-	log.Println("warmup")
+func sequentialRead() (int, error) {
+	mf, err := mmap.OpenMemfile(bigFile)
+	if err != nil {
+		return 0, err
+	}
+	defer mf.Close()
+
+	return countZero(mf.Data()), nil
+}
+
+func parallelRead() (int, error) {
+	mf, err := mmap.OpenMemfile(bigFile)
+	if err != nil {
+		return 0, err
+	}
+	defer mf.Close()
+
+	ch := make(chan int, 4)
+
+	whole := mf.Data()
+	for i := 0; i < 4; i++ {
+		go func(b []byte) {
+			ch <- countZero(b)
+		}(whole[i*unitSize : (i+1)*unitSize-1])
+	}
+
+	cnt := 0
+	for i := 0; i < 4; i++ {
+		select {
+		case c := <-ch:
+			cnt += c;
+		}
+	}
+
+	return cnt, nil
+}
+
+func warmup(f func() (int, error), s string) {
+	log.Println("warming up:", s)
 	for i := 0; i < 5; i++ {
-		f()
+		_, err := f()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
-func benchmark(f func()) {
-	// TODO:
-	log.Println("benchmark")
-	f()
+func benchmark(f func() (int, error), s string) {
+	log.Println("benchmark:", s)
+	for i := 0; i < 10; i++ {
+		log.Println("#", i)
+		_, err := f()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
 func main() {
-	prepareBigFile()
-	warmup(sequentialRead)
-	warmup(parallelRead)
-	benchmark(sequentialRead)
-	benchmark(parallelRead)
+	err := prepareBigFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+	warmup(sequentialRead, "sequential")
+	warmup(parallelRead, "parallel")
+	benchmark(sequentialRead, "sequential")
+	benchmark(parallelRead, "parallel")
 }
